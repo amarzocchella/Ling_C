31de0347f0e7d5b323950bc9cfcc58bf19775a49
Antimo Marzocchella <19870342+amarzocchella@users.noreply.github.com>
2022-02-19 06:31:35 +0100

    Add files via upload

---
+++ Esercizi_OII/Es_12_no_Fz.c
@@ -0,0 +1,21 @@
+/* Consegnare l’elaborato nella cartella condivisa in google drive.
+Svolgere il tracciato dei programmi su carta intestata con nome e cognome e consegnare le foto dei fogli in
+un unico file pdf.
+Alla fine del tracciato scrivere cosa stampa questo programma quando viene eseguito.
+
+Attenzione: fare il tracciato significa scrivere passo passo tutto ciò che fa il programma durante l'esecuzio-
+ne ed evidenziando l'evolversi del contenuto delle variabili ad ogni istruzione eseguita. */
+
+#include <stdio.h>
+#define N 5
+int vett[N];
+main() {
+	  int i;
+    for (i=0;i<N;i++)
+        vett[i]=i+4;
+    for (i=0;i<N;i++)
+        vett[i] = vett[vett[i] % (N-i)];
+
+    for ( i=0;i<N;i++)
+		printf("%d",vett[i]);
+}
